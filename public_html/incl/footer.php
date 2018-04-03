99b32f9e846cc25ca41e81184125cff44b8acd77
kris783c <kristianholmsten@gmail.com>
2018-04-03 13:34:13 +0200

    Footer update 1

--- public_html/incl/footer.php
+++ public_html/incl/footer.php
@@ -1,3 +1,14 @@
+    <footer class="container fStyle">
+      <a href="index.php">Home</a> |
+      <a href="mens.php">Mens</a> |
+      <a href="Women.php">Women</a> |
+      <a href="collections.php">Collections</a> |
+      <a href="contact.php">Contact</a>
+      <br />
+      <p>Copyright © 2014. All Rights Reserved by <span>Fashion Online.</span></p>
+    </footer>
+
+
     <!-- SCRIPTS START -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
