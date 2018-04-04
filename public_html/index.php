6ca875b7da9350a2d5fea7f376f3409bde6c685d
kris783c <kristianholmsten@gmail.com>
2018-04-04 08:41:58 +0200

    Forside Update
    +banner1,2
    + ændringer i index.php og style.css

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,14 @@
 <!--- HEADER INCLUDES START --->
 <?php $PageName = "Forside"; require_once 'incl/header.php'; ?>
 <!--- HEADER INCLUDES END --->
+<section class="container">
+  <div class="row">
+    <img src="content/img/banner1.png" class="col-md-8 imgPadFix" />
+    <img src="content/img/banner2.png" class="col-md-4 imgPadFix imgFix"  />
+  </div>
+</section>
+
+
 
 <!--- HEADER INCLUDES START --->
 <?php require_once 'incl/footer.php'; ?>
