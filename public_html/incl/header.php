5baf8722d69634bb8e1753f455a6aacdd06c8520
Joachim Lind Hadberg <joac1440@elev.techcollege.dk>
2018-04-09 09:22:09 +0200

    he

--- public_html/incl/header.php
+++ public_html/incl/header.php
@@ -10,8 +10,8 @@
 
     <!--- CSS/STYLE --->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
-    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+    <link rel="stylesheet" href="content/owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="content/owlcarousel/owl.theme.default.min.css">
     <link rel="stylesheet" href="content/css/style.css">
 
     <!--- FONTS --->
