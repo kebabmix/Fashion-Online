b79a8936b2f0582f54a9be4988aa9a6e8bef07eb
kris783c <kristianholmsten@gmail.com>
2018-04-09 12:39:17 +0200

    Contact CSS + Contact.php kode :+1:

---
+++ public_html/contact.php
@@ -0,0 +1,23 @@
+<!--- HEADER INCLUDES START --->
+<?php $PageName = "Forside";
+require_once 'incl/header.php'; ?>
+<!--- HEADER INCLUDES END --->
+<!--- TOP BANNER/TEXT START --->
+<section class="container"  >
+<div class="row">
+  <div class="ctcBanner">
+    <h1>contact us</h1>
+    <br />
+    <div class="linebox">
+      <span>Write us a message here</span>
+      <div class="triangle">
+
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<!--- TOP BANNER/TEXT END --->
+<!--- FOOTER INCLUDES START --->
+<?php require_once 'incl/footer.php'; ?>
+<!--- FOOTER INCLUDES END --->
