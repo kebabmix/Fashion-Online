502800a33b9903621595f01c82e9945569967756
cvd2412 <chri798x@elev.techcollege.dk>
2018-04-09 13:59:43 +0200

    cms

--- core/classes/Product.php
+++ core/classes/Product.php
@@ -7,5 +7,48 @@
  */
 
 class Product {
+    public $id;
+    public $thumbnail;
+    public $title;
+    public $description;
+    public $categoryid;
+    public $collectionid;
+
+    public $arrLabels;
+    public $arrFormElms;
+    public $arrValues;
+    private $db;
+
+
+    public function __construct() {
+        global $db;
+        $this->db = $db;
+
+        $this->arrLabels = array(
+            "id" => "Produkt ID",
+            "thumbnail" => "Billede",
+            "title" => "Produktnavn",
+            "desciption" => "Beskrivelse",
+            "category_id" => "Kategori",
+            "collection_id" => "Kollektion"
+        );
+
+        $this->arrFormElms = [
+            "id" => ["hidden", "Produkt ID", FALSE, FILTER_VALIDATE_INT, 0],
+            "thumbnail" => ["text", "Billede", TRUE, FILTER_SANITIZE_STRING, ""],
+            "title" => ["text", "Produktnavn", TRUE, FILTER_SANITIZE_STRING, ""],
+            "desciption" => ["text", "Beskrivelse", TRUE, FILTER_SANITIZE_STRING, ""],
+            "category_id" => ["select", "Kategori", FALSE, FILTER_SANITIZE_STRING, ""],
+            "collection_id" => ["select", "Kollektion", FALSE, FILTER_SANITIZE_STRING, ""]
+        ];
+
+        $this->arrValues = array();
+    }
+
+    public function getlist(){
+        $sql = "SELECT * FROM product " .
+            "WHERE deleted = 0";
+        return $this->db->_fetch_array($sql);
+    }
 
 }
--- public_html/cms/product.php
+++ public_html/cms/product.php
@@ -6,11 +6,33 @@ $strModuleName = "Product";
 switch (strtoupper($mode)) {
     default:
     case "LIST":
-        include DOCROOT . "/cms/content/incl/header.php";
+    include DOCROOT . "/cms/content/incl/header.php";
+    $product = new Product();
+
+    $products = array();
+
+    $columns = [
+        "options" => "",
+        "thumbnail" => "Billede",
+        "title" => "Produktnavn",
+        "description" => "Beskrivelse",
+        "category_id" => "Kategori",
+        "collection_id" => "Kollektion"
+    ];
+    foreach ($product->getlist() as $values) {
+        $values["options"] = htmltool::linkicon("?mode=edit&id=" . $values["id"], "edit", ["id" => 3]) .
+            htmltool::linkicon("?mode=details&id=" . $values["id"], "eye") .
+            htmltool::linkicon("?mode=delete&id=" . $values["id"], "trash");
+
+        $products[] = $values;
+    }
+
+    $p = new listPresenter($columns, $products);
+    echo $p->presentlist();
 
 
-        include DOCROOT . "/cms/content/incl/footer.php";
-        break;
+    include DOCROOT . "/cms/content/incl/footer.php";
+    break;
 
     case "DETAILS":
 
