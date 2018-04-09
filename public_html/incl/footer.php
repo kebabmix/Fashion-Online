fa58e2e14b290b93dc6e7a4c516b547141af07d3
cvd2412 <chri798x@elev.techcollege.dk>
2018-04-09 10:10:15 +0200

    .carousel

--- public_html/incl/footer.php
+++ public_html/incl/footer.php
@@ -16,8 +16,32 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-    <script src="owlcarousel/owl.carousel.min.js"></script>
+    <script src="content/owlcarousel/owl.carousel.min.js"></script>
     <!-- SCRIPTS END -->
-
+    <script>
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            nav:true,
+            autoplay:true,
+            autoplayTimeout:5000,
+            autoplayHoverPause:false,
+            margin: 15,
+            navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+            responsive:{
+                0:{
+                    items:1,
+                    nav:false
+                },
+                600:{
+                    items:3,
+                    nav:false
+                },
+                1000:{
+                    items:5,
+                    nav:true
+                }
+            }
+        })
+    </script>
   </body>
 </html>
--- public_html/index.php
+++ public_html/index.php
@@ -4,56 +4,42 @@ require_once 'incl/header.php'; ?>
 <!--- HEADER INCLUDES END --->
 
 <section class="row">
-  <div class="topnews">
-    <img src="content/img/banner1.png" class="col-md-8 imgPadFix"/>
-    <img src="content/img/banner2.png" class="col-md-4"/>
-  </div>
-  <div class="col-md-8">hej</div>
-  <aside class="col-md-4 sidearea">
-    <img src="content/img/campaign.jpg" alt="" class="col-12">
-    <img src="content/img/campaign-2.jpg" alt="" class="col-12">
-    <form class="newsletter" method="post">
-      <h4>Sign up to our newletter</h4>
-      <hr>
-      <label for="inputEmail" class="col-3">E-Mail</label>
-      <input type="email" name="email" id="inputEmail" style="margin-bottom:1rem;">
-      <label for="inputName" class="col-3">Name</label>
-      <input type="name" name="name" id="inputName" class="">
-      <br>
-      <button class="btn ml-auto" type="submit">Signup</button>
-    </form>
-  </aside>
+    <div class="topnews">
+        <img src="content/img/banner1.png" class="col-md-8 imgPadFix"/>
+        <img src="content/img/banner2.png" class="col-md-4"/>
+    </div>
+    <div class="col-md-8">hej</div>
+    <aside class="col-md-4 sidearea">
+        <img src="content/img/campaign.jpg" alt="" class="col-12">
+        <img src="content/img/campaign-2.jpg" alt="" class="col-12">
+        <form class="newsletter" method="post">
+            <h4>Sign up to our newletter</h4>
+            <hr>
+            <label for="inputEmail" class="col-3">E-Mail</label>
+            <input type="email" name="email" id="inputEmail" style="margin-bottom:1rem;">
+            <label for="inputName" class="col-3">Name</label>
+            <input type="name" name="name" id="inputName" class="">
+            <br>
+            <button class="btn ml-auto" type="submit">Signup</button>
+        </form>
+    </aside>
 </section>
 
 <!--- OWL SLIDER SECTION START --->
-<section class="row">
-  <div class="col-md-12">
-    <div class="owl-carousel owl-theme owl-loaded">
-      <div class="owl-stage-outer">
-        <div class="owl-stage">
-          <div class="owl-item">...</div>
-          <div class="owl-item">...</div>
-          <div class="owl-item">...</div>
+<section class="row carousel">
+    <h1>other products</h1>
+    <div class="col-md-11">
+        <div class="owl-carousel">
+            <div><img src="http://via.placeholder.com/300x200" alt=""></div>
+            <div><img src="http://via.placeholder.com/300x200" alt=""></div>
+            <div><img src="http://via.placeholder.com/300x200" alt=""></div>
+            <div><img src="http://via.placeholder.com/300x200" alt=""></div>
+            <div><img src="http://via.placeholder.com/300x200" alt=""></div>
+            <div><img src="http://via.placeholder.com/300x200" alt=""></div>
+            <div><img src="http://via.placeholder.com/300x200" alt=""></div>
         </div>
-      </div>
-
-      <!--- OWL NAV START --->
-      <div class="owl-nav">
-        <div class="owl-prev">prev</div>
-        <div class="owl-next">next</div>
-      </div>
-      <!--- OWL NAV END --->
-
-      <!--- OWL DOTS START --->
-      <div class="owl-dots">
-        <div class="owl-dot active"><span></span></div>
-        <div class="owl-dot"><span></span></div>
-        <div class="owl-dot"><span></span></div>
-      </div>
-      <!--- OWL DOTS END --->
 
     </div>
-  </div>
 </section>
 <!--- OWL SLIDER SECTION END --->
 
