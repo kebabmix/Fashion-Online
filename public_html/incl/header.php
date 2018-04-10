d7f05bfe514789364a3ba7e91c08042f4bb98127
cvd2412 <chri798x@elev.techcollege.dk>
2018-04-10 08:54:01 +0200

    Implementeret slider

--- public_html/incl/header.php
+++ public_html/incl/header.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="content/css/style.css">
+    <link rel="stylesheet" href="content/owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="content/owlcarousel/owl.theme.default.min.css">
 
     <!--- FONTS --->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
