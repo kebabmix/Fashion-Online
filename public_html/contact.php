e13d020c87fbefc4560dbd229ff2f23796050c0f
kris783c <kristianholmsten@gmail.com>
2018-04-10 08:30:25 +0200

    Contact site :boom:

--- public_html/contact.php
+++ public_html/contact.php
@@ -3,18 +3,33 @@
 require_once 'incl/header.php'; ?>
 <!--- HEADER INCLUDES END --->
 <!--- TOP BANNER/TEXT START --->
-<section class="container"  >
+<section class="container">
 <div class="row">
   <div class="ctcBanner">
     <h1>contact us</h1>
-    <br />
     <div class="linebox">
       <span>Write us a message here</span>
       <div class="triangle">
-
       </div>
     </div>
   </div>
+</div>
+</section>
+<!--- TOP BANNER/TEXT END --->
+<br />
+<br />
+<section class="">
+<div class="col-md-4">
+  <form class="contactForm" method="post">
+      <input type="name" name="name" placeholder="Name" id="inputName">
+      <input type="email" name="email" placeholder="E-Mail" id="inputEmail" style="margin-bottom:1rem;">
+      <textarea placeholder="Message"></textarea>
+      <br>
+      <button class="btn ml-auto" type="submit">Signup</button>
+  </form>
+</div>
+<div class="col-md-8">
+
 </div>
 </section>
 <!--- TOP BANNER/TEXT END --->
