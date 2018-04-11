271c6cae63033543b6cf9a1f4a85e6e3f5a34e74
cvd2412 <chri798x@elev.techcollege.dk>
2018-04-11 15:13:19 +0200

    contact page fix

--- public_html/contact.php
+++ public_html/contact.php
@@ -24,23 +24,49 @@ require_once 'incl/header.php'; ?>
         <form class="contactForm" method="post">
             <input type="name" name="name" placeholder="Name" id="inputName">
             <input type="email" name="email" placeholder="E-Mail" id="inputEmail">
-            <textarea non-re placeholder="Message"></textarea>
-            <br>
-            <button class="btn ml-auto" type="submit">Send us the message</button>
+            <textarea placeholder="Message"></textarea>
+            <button class="btn ml-auto d-block" type="submit">Send us the message</button>
         </form>
     </div>
     <div class="col-md-8 contactInfo">
         <h3>Fashion Online</h3>
-        <br/>
-        <p>
-            Address: Lindholm Brygge 9
-            <br/> zipcode: 9440 norresundby
-            <br/> country: denmark
-            <br/> phone +45 12 34 56 78
-        </p>
+        <table>
+            <tr>
+                <td>Address:&nbsp;</td>
+                <td>Lindholm&nbsp; Brygge&nbsp; 9</td>
+            </tr>
+            <tr>
+                <td>Zipcode:&nbsp;</td>
+                <td>9440&nbsp; Norresundby</td>
+            </tr>
+            <tr>
+                <td>Country:&nbsp;</td>
+                <td>Denmark</td>
+            </tr>
+            <tr>
+                <td>Phone:&nbsp;</td>
+                <td>+45&nbsp; 12&nbsp; 34&nbsp; 56&nbsp; 78</td>
+            </tr>
+        </table>
+        <br>
+        <div id="map"></div>
     </div>
 </section>
 <!--- TOP BANNER/TEXT END --->
 <!--- FOOTER INCLUDES START --->
+<script>
+    function initMap() {
+        var uluru = {lat: 57.066603, lng: 9.899112};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 14,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1_gZQLY6pqZpKMTVrWdzB9wYBgNoKs-c&callback=initMap"></script>
 <?php require_once 'incl/footer.php'; ?>
 <!--- FOOTER INCLUDES END --->
